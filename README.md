3e3eedae694624966cc2bcc55b7f065398d744fc
BrandonTang89 <tangyuhanbrandon@gmail.com>
2026-04-09 09:13:53 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Prism-rs
+This aims to be a Rust implementation of some of the features of the [Prism](https://www.prismmodelchecker.org/) model checker.
+
+## To-do
+- a parser for Prism DTMCs
+- a parser for Prism PCTL formulae
+- a model construction algorithm for DTMC symbolic models
+- a model checking algorithm for PCTL formulae on DTMC symbolic models
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              rust
+              pkgs.rust-analyzer
+              pkgs.python3
+              pkgs.python3Packages.mypy
+              pkgs.uv
+            ];
+          };
+        });
+    };
+}
